b69c2de12a050a72a519ab7af7189f7fb2fbee09
rimanshusingh4 <rimanshusingh4@gmail.com>
2023-10-29 16:30:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useCallback, useState, useEffect,useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [len,setLen] = useState(8);
+  const [numallocate, setNumallocate] = useState(false);
+  const [charallocate, setCharallocate] = useState(false);
+  const [pass,setPass] = useState("");
+
+  const passGenerator = useCallback(() =>{
+    let pass="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numallocate) str+= "0123456789";
+    if(charallocate) str+="@#$&+-";
+    for(let i=1; i<=len; i++){
+      let char = Math.floor(Math.random() * str.length+1);
+      pass += str.charAt(char);
+    }
+    setPass(pass);
+
+
+  }, [len,numallocate,charallocate,setPass])
+  
+  useEffect(()=>{passGenerator()},
+  [len,numallocate,charallocate,passGenerator])
+
+  //UseRef 
+
+  const passRef = useRef(null)
+
+  const copyPassToClip= useCallback(() => {
+    passRef.current?.select()
+    passRef.current?.setSelectionRange(0,49);
+    window.navigator.clipboard.writeText(pass);
+  })
+  return (
+    <>
+      <h1>Password Generator.</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4 mt-4'>
+        <input type='text'
+        value={pass}
+        className='outline-none w-full py-3 px-3'
+        readOnly
+        placeholder='Password'
+        ref={passRef}
+      />
+      <button
+      onClick={copyPassToClip}
+      className='outline-none shrink-0 bg-green-400'>COPY</button>
+      </div>
+      <div className='flex text-xl gap-x-6'>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type='range'
+            min={8}
+            max={50}
+            value={len}
+            onChange={(e)=>{setLen(e.target.value)}}
+            className='cursor-pointer'
+          />
+          <label>Length: {len}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type='checkbox'
+            defaultChecked={numallocate}
+            id='numInput'
+            onChange={()=>setNumallocate((prev) => !prev)}
+          />
+          <label>Number</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type='checkbox'
+            defaultChecked={charallocate}
+            id='numInput'
+            onChange={()=>setCharallocate((prev) => !prev)}
+          />
+          <label>Character</label>
+        </div>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
